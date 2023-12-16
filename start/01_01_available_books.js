c4be66b8d28ae81adf0ffef7be8eacaab1d045ce
Rita Mwaikinda <71730217+ritamwaikinda@users.noreply.github.com>
2023-12-16 05:05:13 +0000

    Books class with setters/getters 1 of 15

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,35 @@
 // Write your code here
+
+class Books {
+  constructor(title, author, isbn, numCopies) {
+    this.title = title,
+    this.author = author,
+    this.isbn = isbn,
+    this.numCopies = numCopies
+  }
+
+  get availability() {
+    return this.getAvailability();
+  }
+  
+  getAvailability() {
+    return this.numCopies === 0 ? "out of stock" : this.numCopies < 10 ? "low stock" : "in stock";
+  }
+
+  sell(numSold = 1) {
+    this.numCopies -= numSold;
+  }
+
+  restock(numToRestock = 5) {
+    this.numCopies += numToRestock;
+  }
+}
+
+ let Titanic = new Books('Titanic', 'Harry Potter', 5012345, 7);
+ console.log(Titanic.author);
+ console.log(Titanic.availability);
+ console.log(Titanic.numCopies);
+ Titanic.sell(3);
+ console.log(Titanic.numCopies);
+ Titanic.restock();
+ console.log(Titanic.numCopies);
