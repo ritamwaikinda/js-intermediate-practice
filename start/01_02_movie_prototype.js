338b3ef9232e2626e7d29f3f50277cfa9cd024b6
Rita Mwaikinda <71730217+ritamwaikinda@users.noreply.github.com>
2023-12-16 08:19:28 +0000

    movie class with object syntax. function syntax coming next.

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,18 @@
 // Write your code here
+
+class Movie {
+  constructor (title, director, genre, releaseYear, rating) {
+    this.title = title,
+    this.director = director,
+    this.genre = genre,
+    this.releaseYear = releaseYear,
+    this.rating = rating
+  }
+
+  getOverview() {
+    console.log(`${this.title}, a ${this.genre} film directed by ${this.director} was releasedin ${this.releaseYear}. It received a rating of ${this.rating}.`)
+  }
+}
+
+const Scarface = new Movie("Scarface", "Scorcese", "Crime Drama", "1989", "4.8/5");
+console.log(Scarface.getOverview());
