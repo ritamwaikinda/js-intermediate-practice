354f27e3f438ee42d8e06c8281215ff31ac2ada4
Rita Mwaikinda <71730217+ritamwaikinda@users.noreply.github.com>
2023-12-18 04:01:27 +0000

    function syntax and class syntax for object creation

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,18 +1,42 @@
 // Write your code here
 
-class Movie {
-  constructor (title, director, genre, releaseYear, rating) {
-    this.title = title,
-    this.director = director,
-    this.genre = genre,
-    this.releaseYear = releaseYear,
-    this.rating = rating
-  }
+// CLASS SYNTAX
 
-  getOverview() {
-    console.log(`${this.title}, a ${this.genre} film directed by ${this.director} was releasedin ${this.releaseYear}. It received a rating of ${this.rating}.`)
+// class Movie {
+//   constructor (title, director, genre, releaseYear, rating) {
+//     this.title = title,
+//     this.director = director,
+//     this.genre = genre,
+//     this.releaseYear = releaseYear,
+//     this.rating = rating
+//   }
+
+//   getOverview() {
+//     console.log(`${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`)
+//   }
+// }
+
+// FUNCTION SYNTAX
+
+//capital M indicates that the function must be called with the 'new' keyword
+function Movie(title, director, genre, releaseYear, rating) {
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.releaseYear = releaseYear;
+  this.rating = rating;
+  this.getOverview = function () {
+    console.log(`${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`)
   }
 }
+
+//to add a function to the PROTOTYPE, use the constructor function name, followed by the 'prototype' keyword, then the function name you want to add. This would be optimal. Declare the function on the prototype so that each new instance of the function/movie object does NOT recreate this function. Comment out function in the above constructor function, then uncomment the function below, save it, and run.
+
+// Movie.prototype.getOverview = function() {
+//   return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`
+// }
+
+
 
 const Scarface = new Movie("Scarface", "Scorcese", "Crime Drama", "1989", "4.8/5");
 console.log(Scarface.getOverview());
